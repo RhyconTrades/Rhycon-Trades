92702e7d7ec89f9cbde95d6d1da723a5e1911e24
HoussemDegachi <houssemdegach@gmail.com>
2023-05-07 16:38:07 +0100

    v 1.05

--- src/compnents/Features.jsx
+++ src/compnents/Features.jsx
@@ -12,16 +12,16 @@ function Features() {
                 <h2 className="section-title features-title"> <span className="purple">Maximize</span> Your ROI: Discover Our <span className="purple">High-Value</span> Services</h2>
                 <ul className="feature--list">
                   <li className="feature--list__item">
-                    <p><FontAwesomeIcon className='purple' icon='fa fa-arrow-right' /> High potantial returns </p>
+                    <p><FontAwesomeIcon className='purple' icon='fa fa-arrow-right' /> Make better trades with <b className="purple">accuracy</b></p>
                   </li>
                   <li className="feature--list__item">
-                    <p><FontAwesomeIcon className='purple' icon='fa fa-arrow-right' /> Greater <b className="purple">control over funds</b></p>
+                    <p><FontAwesomeIcon className='purple' icon='fa fa-arrow-right' /> <b className="purple">save time</b> analyzing data</p>
                   </li>
                   <li className="feature--list__item">
-                    <p><FontAwesomeIcon className='purple' icon='fa fa-arrow-right' /> <b className="purple">Risk management</b> and position sizing </p>
+                    <p><FontAwesomeIcon className='purple' icon='fa fa-arrow-right' /> take the <b className="purple">guesswork out</b> of trading</p>
                   </li>
                   <li className="feature--list__item">
-                    <p><FontAwesomeIcon className='purple' icon='fa fa-arrow-right' /> Protection against <b className="purple"> inflation</b></p>
+                    <p><FontAwesomeIcon className='purple' icon='fa fa-arrow-right' /> <b className="purple">gain</b> an edge in the market</p>
                   </li>
                 </ul>
                 <Link to='/products'><button className='features__btn'>enroll now</button></Link>
